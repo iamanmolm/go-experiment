4d8d1b4bd4ebeb417c561e407f2bdf86de5deff0
Anmol Maheshwari <anmol.maheshwari@harness.io>
2022-06-28 16:43:22 +0530

    go-experiment

--- main.go
+++ main.go
@@ -11,9 +11,7 @@ func main() {
 	var remainingTickets uint = 50
 	var bookings []string
 
-	fmt.Println("Welcome to our", applicationName, "application")
-	fmt.Println("We have total of", applicationTickets, "tickets and", remainingTickets, "are still remaining")
-	fmt.Println("Get your tickets to explore our application further")
+	greetUsers(applicationName, applicationTickets, remainingTickets)
 
 	for {
 		var firstName string
@@ -30,21 +28,49 @@ func main() {
 		fmt.Println("Enter number of tickets:")
 		fmt.Scan(&userTickets)
 
-		bookings = append(bookings, firstName+" "+lastName)
-		remainingTickets = remainingTickets - userTickets
+		//Data Validation
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
 
-		fmt.Printf("Thank you %v %v for booking %v tickets, you will receive a email confirmation on %v\n", firstName, lastName, userTickets, email)
-		fmt.Println("We have total of", applicationTickets, "tickets and", remainingTickets, "are still remaining")
+		if isValidName && isValidEmail && isValidTicketNumber {
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
 
-		bookedFirstNames := []string{}
-		for _, booking := range bookings {
-			bookedFirstNames = append(bookedFirstNames, strings.Fields(booking)[0])
-		}
-		fmt.Printf("The first name of all the bookings: %v\n", bookedFirstNames)
+			fmt.Printf("Thank you %v %v for booking %v tickets, you will receive a email confirmation on %v\n", firstName, lastName, userTickets, email)
+			fmt.Println("We have total of", applicationTickets, "tickets and", remainingTickets, "are still remaining")
 
-		if remainingTickets == 0 {
-			fmt.Println("Tickets for our application is booked out. Come back in next slot")
-			break
+			bookedFirstNames := getFirstNames(bookings)
+			fmt.Printf("The first name of all the bookings: %v\n", bookedFirstNames)
+
+			if remainingTickets == 0 {
+				fmt.Println("Tickets for our application is booked out. Come back in next slot")
+				break
+			}
+		} else {
+			if !isValidName {
+				fmt.Println("first name or last name you entered is too short!")
+			}
+			if !isValidEmail {
+				fmt.Println("e-mail address you entered doesn't contain @ symbol!")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("Number of ticket you entered is either less than 0 or greater than remaining tickets!")
+			}
 		}
 	}
+}
+
+func greetUsers(applicationName string, applicationTickets uint, remainingTickets uint) {
+	fmt.Println("Welcome to our", applicationName, "application")
+	fmt.Println("We have total of", applicationTickets, "tickets and", remainingTickets, "are still remaining")
+	fmt.Println("Get your tickets to explore our application further")
+}
+
+func getFirstNames(bookings []string) []string {
+	bookedFirstNames := []string{}
+	for _, booking := range bookings {
+		bookedFirstNames = append(bookedFirstNames, strings.Fields(booking)[0])
+	}
+	return bookedFirstNames
 }
